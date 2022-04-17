f49bb4a714bb343d3ce6c2018da2124b4d186903
naturenomad <76237641+naturenomad@users.noreply.github.com>
2022-04-18 11:22:05 +1200

    Update simplesqlqueries.sql

--- simplesqlqueries.sql
+++ simplesqlqueries.sql
@@ -1,35 +1,36 @@
-##########################################################################
 
-# Solutions to the PostgreSql exercises listed at https://pgexercises.com/
-# Listed below in sequential order
+-- ##########################################################################
 
-############################
-#### Simple SQL Queries ####
+-- Solutions to the PostgreSql exercises listed at https://pgexercises.com/
+-- Listed below in sequential order
 
-https://pgexercises.com/questions/basic/
+-- ############################
+-- #### Simple SQL Queries ####
 
-# This category deals with the basics of SQL. It covers select and where clauses, case expressions, unions, and a few other odds and ends. 
+-- https://pgexercises.com/questions/basic/
 
-# Retrieve everything from a table
+-- This category deals with the basics of SQL. It covers select and where clauses, case expressions, unions, and a few other odds and ends. 
+
+-- Retrieve everything from a table
 select * from cd.facilities;
 
-# Retrieve specific columns from a table
+-- Retrieve specific columns from a table
 select name, membercost from cd.facilities;
 
-#  Control which rows are retrieved
+-- Control which rows are retrieved
 select * from cd.facilities where membercost > 0;
 
-# Control which rows are retrieved - part 2
+-- Control which rows are retrieved - part 2
 select facid, name, membercost, monthlymaintenance 
 from cd.facilities where membercost > 0 and membercost < (monthlymaintenance/50);
 
-# Basic string searches
+-- Basic string searches
 select * from cd.facilities where name like '%Tennis%';
 
-# Matching against multiple possible values
+-- Matching against multiple possible values
 select * from cd.facilities where facid in (1,5);
 
-# Classify results into buckets
+-- Classify results into buckets
 select name, 
     case 
         when (monthlymaintenance > 100) then 'expensive'
@@ -37,22 +38,22 @@ select name,
     end as cost
 from cd.facilities;
 
-# Working with dates
+-- Working with dates
 select memid, surname, firstname, joindate from cd.members
 where joindate >= '2012-09-01';
 
-# Removing duplicates, and ordering results
+-- Removing duplicates, and ordering results
 select distinct(surname) from cd.members order by surname limit 10;
 
-# Combining results from multiple queries
+-- Combining results from multiple queries
 select surname from cd.members
 union
 select name from cd.facilities;
 
-# Simple aggregation
+-- Simple aggregation
 select max(joindate) from cd.members;
 
-# More aggregation
+-- More aggregation
 select firstname, surname, joindate from cd.members
 where joindate=(select max(joindate) from cd.members);
 
