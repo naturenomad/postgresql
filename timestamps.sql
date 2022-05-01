ec79fdfeece106dc14363dbbd47f05db330f6fdb
naturenomad <76237641+naturenomad@users.noreply.github.com>
2022-05-02 05:30:37 +1200

    Update timestamps.sql

--- timestamps.sql
+++ timestamps.sql
@@ -91,6 +91,58 @@ from
 (select generate_series('2012-01-01', '2012-12-01', interval '1 month') as month) ts;
 
 
------------------------------------------------
--- tbc.....
-	
+-----------------------------------------------------
+-- Work out the number of days remaining in the month
+-- For any given timestamp, work out the number of days remaining in the month. 
+-- The current day should count as a whole day, regardless of the time. 
+-- Use '2012-02-11 01:00:00' as an example timestamp for the purposes of making the answer. 
+-- Format the output as a single interval value.
+-- Hint Take a look at the DATE_TRUNC function 
+
+-- Solve by going to beginning of month plus one month, 
+-- and subtracting the date to get days remaining -
+select (date_trunc('month', timestamp '2012-02-11 01:00:00') 
+		+ interval '1 month')
+		- date_trunc('day', timestamp '2012-02-11 01:00:00') 
+		as remaining;
+
+
+------------------------------------
+-- Work out the end time of bookings
+-- Return a list of the start and end time of the last 10 bookings 
+-- (ordered by the time at which they end, followed by the time at which they start) 
+-- in the system. 
+select starttime, 
+starttime + (interval '1 hour' * (slots*0.5)) as endtime
+from cd.bookings
+order by endtime desc, starttime desc
+limit 10;
+
+-- Alternative : slots * (interval '30 minutes')
+
+
+--------------------------------------------
+-- Return a count of bookings for each month
+select date_trunc('month', starttime) as month, count(*)
+from cd.bookings
+group by month
+order by month;
+
+
+-----------------------------------------------------------------
+-- Work out the utilisation percentage for each facility by month 
+-- Sorted by name and month, rounded to 1 decimal place. 
+-- Opening time is 8am, closing time is 8.30pm. 
+-- Treat every month as a full month, 
+-- regardless of if there were some dates the club was not open. 
+select f.name, date_trunc('month', b.starttime) as month,
+round((sum(0.5 * b.slots) /
+(date_part('days', 
+  (date_trunc('month', b.starttime) + interval '1 month')
+  - date_trunc('month', b.starttime)) * 12.5) * 100)::numeric, 1) as utilisation
+from cd.bookings b
+inner join cd.facilities f
+on b.facid=f.facid
+group by name, month
+order by name, month;
+
